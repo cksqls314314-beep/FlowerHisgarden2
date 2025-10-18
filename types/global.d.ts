0afed7740bfec7ebee18a4bace492eb36e6bd33e
CB <a123@123s-MacBook-Air.local>
2025-10-18 21:16:30 +0900

    chore(vercel): force build ignoring TS/ESLint + add global type refs

---
+++ types/global.d.ts
@@ -0,0 +1,4 @@
+// Extra global type references to help Vercel's TS find Next types
+/// <reference types="next" />
+/// <reference types="next/image-types/global" />
+/// <reference types="next/types/global" />
